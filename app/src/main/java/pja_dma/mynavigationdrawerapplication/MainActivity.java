4ca8390a5427a5ff5dea6781cdf4395236f225b4
PeterJ504 <peterjalderton@gmail.com>
2016-08-11 23:00:21 -0600

    Hambuger icon to control ActionBarDrawer Toggle

--- app/src/main/java/pja_dma/mynavigationdrawerapplication/MainActivity.java
+++ app/src/main/java/pja_dma/mynavigationdrawerapplication/MainActivity.java
@@ -1,13 +1,38 @@
 package pja_dma.mynavigationdrawerapplication;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Enables code to listen to clicks in the action bar. It also controls it using
+        // the "hamburger" icon.
+
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // show 'hamburger icon
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    // Capture whether the 'hambuger' icon was selected
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (toggle.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
     }
 }
